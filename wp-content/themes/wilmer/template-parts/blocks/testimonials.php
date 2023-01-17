b6ad64b15b57e9a429fca032b127403250f62ef3
Arturs Juhnevics <arturs@minimo.lv>
2023-01-17 17:16:26 +0200

    css style

---
+++ wp-content/themes/wilmer/template-parts/blocks/testimonials.php
@@ -0,0 +1,22 @@
+<div class="wp-block-qi-blocks-column qodef-gutenberg-column qodef-block-b6e815eb qodef-vertical-align--top"><div class="qodef-gutenberg-column-inner">
+<section class="wp-block-qi-blocks-columns qodef-gutenberg-section qodef--template qodef-block-3f786169 qodef-col-num--1 qodef-col-layout--100 qodef-gutter--no qodef-height--min-height qodef-position--middle qodef-content--full-width"><div class="qodef-section-background-overlay"></div><div class="qodef-gutenberg-row">
+<div class="wp-block-qi-blocks-column qodef-gutenberg-column qodef-block-931c8484 qodef-vertical-align--middle qodef-horizontal-align--start"><div class="qodef-gutenberg-column-inner">
+<div class="wp-block-qi-blocks-icon-with-text qodef-block-container qodef-block-7c65f19d"><div class="qi-block-icon-with-text qodef-block qodef-m qodef-layout--top qodef-column-responsive--never qodef--appeared"><div class="qodef-m-icon-wrapper"><div class="qodef-m-icon-holder"><svg xml:space="preserve" viewBox="0 0 69 55" y="0px" x="0px" xmlns="http://www.w3.org/2000/svg" version="1.1">
+<g>
+<path d="M0,30.759c0-9.233,2.428-16.668,7.285-22.306C12.141,2.82,19.223,0,28.531,0v12.834
+                                    c-7.152,0-10.724,4.618-10.724,13.852v3.056h12.95V55H4.249C1.416,46.852,0,38.774,0,30.759z M38.446,30.759
+                                    c0-9.233,2.393-16.668,7.183-22.306C50.415,2.82,57.466,0,66.774,0v12.834c-7.016,0-10.522,4.618-10.522,13.852v3.056H69V55H42.493
+                                    C39.792,46.718,38.446,38.637,38.446,30.759z" fill="currentColor"></path>
+</g>
+</svg></div></div></div></div>
+
+
+
+<div class="wp-block-qi-blocks-testimonials-list qodef-block-container qodef-block-f2799a0d qodef-width--custom qodef-width--mobile--full-width" data-width="506px"><div class="qi-block-testimonials-list qodef-block qodef-m qodef-gutenberg-section qodef--columns qodef-col-num--1 qodef-responsive--custom qodef-col-num--1440--1 qodef-col-num--1366--1 qodef-col-num--1024--1 qodef-col-num--768--1 qodef-col-num--680--1 qodef-col-num--480--1 qodef-item-layout--side-with-image"><div class="qodef-gutenberg-row"><div class="qodef-gutenberg-column qodef-e qi-repeater-item-f2799a0d0"><div class="qodef-e-inner"><div class="qodef-e-side"><div class="qodef-e-media-image"><img decoding="async" src="https://wilmer.qodeinteractive.com/gutenberg/wp-content/uploads/2019/01/h3-testimonials-02.jpg" alt=""></div><div class="qodef-e-quote"><svg class="qodef-e-quote-icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 190.5 148" xml:space="preserve"><g><path d="M37.7,146.3L2.1,124.6C19.3,100,28.2,74.1,28.8,46.7V2.3H90v38.8c0,19.3-5,38.8-15.1,58.4C64.9,119,52.5,134.6,37.7,146.3z M133.7,146.3l-35.6-21.7c17.2-24.5,26.2-50.5,26.8-77.9V2.3h61.2v38.8c0,19.3-5,38.8-15.1,58.4C160.9,119,148.5,134.6,133.7,146.3z"></path></g></svg></div></div><div class="qodef-e-content"><h4 class="qodef-e-title">Nathan Marshall</h4><p class="qodef-e-text">Aliquam lorem ante, dapibus in, viverra nisi quis, feugiat a, tellus. Phasellus viverra deo nulla ut metus varius qui laoreet. Quisque sum rutrum. Aenean imperdiet. Etiam ultricies cum sociis.</p></div></div></div></div></div></div>
+</div></div>
+</div></section>
+
+
+
+<div class="wp-block-qi-blocks-divider qodef-block-container qodef-block-f6dfe65a"><div class="qi-block-divider qodef-block qodef-m qodef-layout--standard qodef-position--center"><div class="qodef-m-line"></div></div></div>
+</div></div>z
