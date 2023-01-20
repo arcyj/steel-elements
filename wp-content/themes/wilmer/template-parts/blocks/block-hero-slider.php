7f0bb9bd3b73dde6b8c369b1ef1fc34a6c568181
Arturs Juhnevics <arturs@minimo.lv>
2023-01-20 12:33:14 +0200

    wip

--- wp-content/themes/wilmer/template-parts/blocks/block-hero-slider.php
+++ wp-content/themes/wilmer/template-parts/blocks/block-hero-slider.php
@@ -4,17 +4,21 @@ $title = get_field('title');
 $text = get_field('text');
 $image = get_field('image');
 
-echo '<div class="hero-slider">';
-  echo '<div class="hero-slider-item" style="background-image: url(' . wp_get_attachment_url($image['ID'], 'large', null) . ')">';
-    echo '<div class="hero-slider-item-content">';
-      echo '<div class="hero-slider-item-info">';
-        if (! empty($title)) {
-            echo '<h3>' . $title . '</h3>';
-        }
-        if (! empty($text)) {
-            echo '<p>' . $text . '</p>';
-        }
-      echo '</div>';
-    echo '</div>';
-  echo '</div>';
-echo '</div>';
+?>
+
+<div class="hero-slider">
+  <div class="hero-slider-item" style="background-image: url('<?php echo wp_get_attachment_url($image['ID'], 'large', null) ?>')">
+    <div class="hero-slider-bg"></div>
+    <div class="hero-slider-item-content">
+      <div class="hero-slider-item-info">
+        <?php if (! empty($title)) { ?>
+            <h3> <?php echo $title ?></h3>
+        <?php } ?>
+        <?php if (! empty($text)) { ?>
+            <p> <?php echo $text ?> </p>
+        <?php } ?>
+
+      </div>
+    </div>
+  </div>
+</div>
