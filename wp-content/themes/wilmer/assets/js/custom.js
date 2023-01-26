f16f46b3277bc8b0da5ace793fc436904960fb43
Arturs Juhnevics <arturs@minimo.lv>
2023-01-26 12:46:25 +0200

    lang switcher

--- wp-content/themes/wilmer/assets/js/custom.js
+++ wp-content/themes/wilmer/assets/js/custom.js
@@ -2,8 +2,7 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
   autoHeight: true,
-  autoplay: {
-    delay: 5000,
+  autoplay: { delay: 3000,
   },
   pagination: {
     el: ".swiper-pagination",
