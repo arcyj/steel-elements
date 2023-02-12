7bb06c99bc6cbfe9d0ec0f1fa6842d3d47fcb6ae
Arturs Juhnevics <arturs@minimo.lv>
2023-02-12 11:07:49 +0200

    text banner text

--- wp-content/themes/wilmer/template-parts/blocks/block-text-banner.php
+++ wp-content/themes/wilmer/template-parts/blocks/block-text-banner.php
@@ -3,6 +3,7 @@
 $url = get_field('url');
 $subtitle = get_field('subtitle');
 $title = get_field('title');
+$text = get_field('text');
 
 ?>
 
@@ -11,6 +12,7 @@ $title = get_field('title');
     <div class="text-banner-content">
       <p class="text-banner-subtitle"><?php echo $subtitle; ?></p>
       <h4 class="text-banner-title"><?php echo $title; ?></h4>
+      <p class="text-banner-text"><?php echo $text; ?></p>
       <a class="text-banner-button" href="<?php echo $url ?>"><span class="qodef-m-icon" style="width: auto;"><span class="qodef-m-icon-inner"><svg viewBox="0 0 13 12" height="12" width="13" xmlns="http://www.w3.org/2000/svg">
         <path d="M1.044,6.144A0.625,0.625,0,0,1,1.493,5.07H5.868V0.695a0.625,0.625,0,1,1,1.25,0V5.07h4.375a0.625,0.625,0,1,1,0,1.25H7.118V10.7a0.625,0.625,0,1,1-1.25,0V6.32H1.493A0.608,0.608,0,0,1,1.044,6.144Z"></path>
         </svg>
